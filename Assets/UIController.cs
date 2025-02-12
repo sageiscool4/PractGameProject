3789c79cdb552687423b438e4f648062b8a1349d
sageiscool4 <ozerovavi13@gmail.com>
2025-02-12 14:23:01 -0600

    same thing

--- Assets/UIController.cs
+++ Assets/UIController.cs
@@ -9,33 +9,50 @@ public class UIController : MonoBehaviour
     [SerializeField] GameObject textBox;
     [SerializeField] TextMeshProUGUI characterText;
     [SerializeField] GameObject replyButton;
-    [SerializeField] Button replyBButton;
+    [SerializeField] TextMeshProUGUI replyBButton;
     [SerializeField] Animator anim;
+    [SerializeField] int numTimesText = 0;
 
     // Start is called before the first frame update
     void Start()
     {
        textBox.SetActive(false);
        replyButton.SetActive(false);
+        StartCoroutine(MomSpeakingBegin(4));
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        StartCoroutine(MomSpeaking(4));
+        
     }
 
-    IEnumerator MomSpeaking(int bSecs)
+    IEnumerator MomSpeakingBegin(int bSecs)
     {
         yield return new WaitForSeconds(bSecs);
 
         textBox.SetActive(true);
         replyButton.SetActive(true);
-        
-        yield return new WaitForSeconds(5);
 
-        anim.SetBool("isUpset", true);
+        numTimesText++;
 
-        characterText.text = "There's a big corporation around here. \nThey're trying to make us go bankrupt!";
+    }
+
+  
+
+   public void replyButtonPressed()
+    {
+        switch (numTimesText)
+        {
+            case 1:
+                anim.SetBool("isUpset", true);
+
+                characterText.text = "There's a big corporation around here. \nThey're trying to make us go bankrupt!";
+                replyBButton.text = "...";
+                numTimesText++;
+                break;
+
+        }
     }
 }
